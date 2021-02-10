773d18a373ae63b88c55d948f0f3deea7ed690ed
Owonin <RinGrayRat@yandex.ru>
2021-02-10 09:06:16 +0300

    first branch commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ import javax.script.ScriptException;
 public class Main {
     public static void main(String arg[]){
             //TODO Интерфейс еще не готов
+            System.out.println("Hi, user");
     }
 }
