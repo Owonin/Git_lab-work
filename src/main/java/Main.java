aa14baf1b858e87529112f38027f80a17f9d5588
Owonin <RinGrayRat@yandex.ru>
2021-02-23 14:20:52 +0300

    gitignore added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ import javax.script.ScriptException;
 
 public class Main {
     public static void main(String arg[]){
-        RiemannSum sum = new RiemannSum(0,2, "sin(x-1)");
-        System.out.println(sum.calculate());
+        RiemannSum sum = new RiemannSum(0,2, "cos(x*x)");
+        System.out.println(sum.calculate() + " "+ sum.getRate());
 
     }
 }
